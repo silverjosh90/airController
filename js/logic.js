cbc5f87f307037b1cd9056375a0bdf3309b635b4
Josh Silverstein <silverjosh90@gmail.com>
2015-11-20 13:22:11 -0700

    Added unfactored code for reference

--- js/logic.js
+++ js/logic.js
@@ -26,3 +26,51 @@ function fareMe() {
   cost+=bagFee
   total.innerHTML=cost;
 }
+
+/*  if(cityChoice == 3.0) {
+    cost+=250
+  }
+  else if(cityChoice == 3.1) {
+    cost+=545
+  }
+  else if(cityChoice == 2.5) {
+    cost+=350
+  }
+  if(wifi.checked== true) {
+  cost+=12
+  }
+
+  var bagFee = Number(bags.value) * 25
+  cost+=bagFee
+
+  total.innerHTML=cost;
+} */
+  /* if(departures.value == 1 && arrivals.value == 1) {
+  // if(departures.value === arrivals.value) {
+    console.log("this is also working");
+    cost += 0;
+  }
+  else if(departure.value == 1 && arrival.value == 2) {
+    cost += 350
+  }
+  else if(departure.value == 1 && arrival.value == 3) {
+    cost += 250
+  }
+  else if(departure.value == 2 && arrival.value == 1) {
+    cost += 350
+  }
+  else if(departure.value == 2 && arrival.value == 2) {
+    cost += 0
+  }
+  else if(departure.value == 2 && arrival.value == 3) {
+    cost += 545
+  }
+  else if(departure.value == 3 && arrival.value == 1) {
+    cost += 250
+  }
+  else if(departure.value == 3 && arrival.value == 2) {
+    cost += 545
+  }
+  else if(departure.value == 3 && arrival.value == 3) {
+    cost += 0
+  } */
