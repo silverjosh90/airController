de8402bc5d1c7ed500a6d6a13a950502ac995953
Sean Brage <seanmbrage@me.com>
2015-11-20 13:01:03 -0700

    split js into two files, tested and verified

--- js/script.js
+++ js/script.js
@@ -5,37 +5,4 @@ var seating= document.getElementById("seating")
 var button = document.getElementById("button")
 var total = document.getElementById('cost')
 var wifi= document.getElementById("wifi")
-
-
 button.addEventListener("click", fareMe, false)
-function fareMe() {
-  var cityChoice = Number(departures.value) + Number(arrivals.value)
-  var cost = 0;
-  switch(cityChoice) {
-    case 3.0:
-      cost+=250;
-      break;
-    case 3.1:
-      cost+=545
-    case 2.5:
-      cost+=350
-      break;
-  }
-  switch(Number(seating.value)) {
-    case 1:
-      cost+=200
-      break;
-    case 2:
-      cost+=500
-      break;
-  }
-
-  if(wifi.checked== true) {
-  cost+=12
-  }
-
-  var bagFee = Number(bags.value) * 25
-  cost+=bagFee
-
-  total.innerHTML=cost;
-}
