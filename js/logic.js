389c02d826fb55119035c2808b9b14761a55cfb4
Sean Brage <seanmbrage@me.com>
2015-11-20 15:31:30 -0700

    beautified all code in all files

--- js/logic.js
+++ js/logic.js
@@ -1,56 +1,63 @@
 var cost = 0;
-function finalCost(){
+
+function finalCost() {
   cityVal();
   seatVal();
   wifiVal();
   bagVal();
   discountVal();
-  total.innerHTML='\$' + cost;
+  total.innerHTML = '\$' + cost;
   cost = 0
 }
+
 function cityVal() {
   var cityChoice = Number(departures.value) + Number(arrivals.value)
-  switch(cityChoice) {
+  switch (cityChoice) {
     case 3.0:
-      cost+=250;
+      cost += 250;
       break;
     case 3.1:
-      cost+=545
+      cost += 545
     case 2.5:
-      cost+=350
+      cost += 350
       break;
   }
 }
+
 function seatVal() {
-  switch(Number(seating.value)) {
+  switch (Number(seating.value)) {
     case 1:
-      cost+=200
+      cost += 200
       break;
     case 2:
-      cost+=500
+      cost += 500
       break;
   }
 }
-function wifiVal(){
-  if(wifi.checked== true) {
-  cost+=12
+
+function wifiVal() {
+  if (wifi.checked == true) {
+    cost += 12
   }
 }
-function bagVal(){
+
+function bagVal() {
   var bagFee = Number(bags.value) * 25
-  cost+=bagFee
+  cost += bagFee
 }
-function discountVal(){
+
+function discountVal() {
   var discountCode = (discount.value)
-  switch(discountCode) {
+  switch (discountCode) {
     case 'g18':
-    cost-=100
-    break;
+      cost -= 100
+      break;
     case 'g15':
-    cost+=5000
+      cost += 5000
   }
 }
+
 function formReset() {
   cost = 0;
-  total.innerHTML='\$' + cost;
+  total.innerHTML = '\$' + cost;
 }
