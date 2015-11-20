8ca09ee47dce59ddea994ecadd4d77ffc736c696
Sean Brage <seanmbrage@me.com>
2015-11-20 12:33:23 -0700

    change 'classe' to 'seating'

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var arrivals = document.getElementById('arrival')
 var total = document.getElementById('cost')
 var wifi= document.getElementById("wifi")
 var bags = document.getElementById("bags")
-var classe= document.getElementById("class")
+var seating= document.getElementById("class")
 
 
 button.addEventListener("click", fareMe, false)
@@ -21,7 +21,7 @@ function fareMe() {
       cost+=350
       break;
   }
-  switch(Number(classe.value)) {
+  switch(Number(seating.value)) {
     case 1:
       cost+=200
       break;
@@ -39,51 +39,3 @@ function fareMe() {
 
   total.innerHTML=cost;
 }
-
-/*  if(cityChoice == 3.0) {
-    cost+=250
-  }
-  else if(cityChoice == 3.1) {
-    cost+=545
-  }
-  else if(cityChoice == 2.5) {
-    cost+=350
-  }
-  if(wifi.checked== true) {
-  cost+=12
-  }
-
-  var bagFee = Number(bags.value) * 25
-  cost+=bagFee
-
-  total.innerHTML=cost;
-} */
-  /* if(departures.value == 1 && arrivals.value == 1) {
-  // if(departures.value === arrivals.value) {
-    console.log("this is also working");
-    cost += 0;
-  }
-  else if(departure.value == 1 && arrival.value == 2) {
-    cost += 350
-  }
-  else if(departure.value == 1 && arrival.value == 3) {
-    cost += 250
-  }
-  else if(departure.value == 2 && arrival.value == 1) {
-    cost += 350
-  }
-  else if(departure.value == 2 && arrival.value == 2) {
-    cost += 0
-  }
-  else if(departure.value == 2 && arrival.value == 3) {
-    cost += 545
-  }
-  else if(departure.value == 3 && arrival.value == 1) {
-    cost += 250
-  }
-  else if(departure.value == 3 && arrival.value == 2) {
-    cost += 545
-  }
-  else if(departure.value == 3 && arrival.value == 3) {
-    cost += 0
-  } */
